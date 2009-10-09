aac13f1f81607d93a59d05cb3027b7ea7d07f7b0
cmoullet <cmoullet>
2009-10-09 07:49:56 +0000

    Add Studio Beta doc, OGRS and Tiling. Modify MapFish Logo, minr css fixes
    git-svn-id: http://svn.mapfish.org/svn/mapfish/website/trunk@3064 25cca10d-0837-0410-9e4f-92b931fda1bc

--- conf.py
+++ conf.py
@@ -112,7 +112,7 @@ html_title = "MapFish"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "_static/img/mapfish.png"
+html_logo = "_static/img/mapfish_large.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
